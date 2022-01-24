732344f4b75ffd89de72157147552365a3ea1bec
Dmitry Medvedew <bikishovd>
2022-01-24 12:56:27 +0700

    add update user

--- internal/app/app.go
+++ internal/app/app.go
@@ -8,21 +8,38 @@ import (
 )
 
 func Start(db *gorm.DB) {
-	create(db)
+	selectAndUpdate(db)
 }
 
 func create(db *gorm.DB) {
-	u := &models.User{
-		Name: "test",
-		Age:12,
+	user := &models.User{
+		Name:     "test",
+		Age:      12,
 		IsVerify: true,
 	}
 
-	result := db.Create(u)
+	result := db.Create(user)
 
 	if result.Error != nil {
 		panic(result.Error)
 	}
 
-	fmt.Printf("User created with id %d", u.ID)
+	fmt.Printf("User created with id %d", user.ID)
+}
+
+func selectAndUpdate(db *gorm.DB) {
+	user := &models.User{}
+
+	db.Where("name = ?", "test").First(&user)
+
+	if user.ID > 0 {
+		user.Name = "Dmitry"
+		db.Save(user)
+	}
+
+	fmt.Printf("User updated with id %d", user.ID)
+
+	// or this way but all records
+	// db.Model(&models.User{}).Where("name = ?", "test").Update("name", "Dmitry")
+
 }
