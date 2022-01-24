eb0692e571aba750e27edb3c67c76bced5c57ec9
Dmitry Medvedew <bikishovd>
2022-01-24 21:31:06 +0700

    :sparkles: add aggreate query

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"time"
 
 	"github.com/brianvoe/gofakeit/v6"
@@ -23,11 +25,40 @@ func main() {
 
 	sess, err := mongo.Dial("mongodb://localhost:27017/test")
 
-	CreateUsers(sess)
+	AggregateUsers(sess)
 
 	if err != nil {
 		panic(err)
 	}
+}
+
+func AggregateUsers(sess *mongo.Session) {
+
+	var result []bson.M
+
+	pipeline := []bson.M{
+		{
+			"$group": bson.M{
+				// TODO fix There are  nested object
+				"_id": bson.M{
+					"sex": "$sex", "model": "$smartphone.model",
+				},
+				"count": bson.M{
+					"$sum": 1,
+				},
+			},
+		},
+	}
+
+	err := sess.DB("").C("users").Pipe(pipeline).All(&result)
+
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	file, _ := json.MarshalIndent(result, "", " ")
+
+	_ = ioutil.WriteFile("data.json", file, 0644)
 
 }
 
@@ -39,7 +70,7 @@ func CreateUsers(sess *mongo.Session) {
 			DateOfBirth: gofakeit.Date().Unix() - int64(time.Hour)*18,
 		}
 
-		user.Smartphone.Model = gofakeit.Word()
+		user.Smartphone.Model = gofakeit.RandomString([]string{"Apple", "Mui", "Redmi"})
 		user.Smartphone.Vendor = gofakeit.Number(10, 100)
 
 		err := Store(sess, user)
