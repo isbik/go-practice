65636d4423ee505350516536cadd6d3e18bbeb5f
Dmitry Medvedew <bikishovd>
2022-02-01 17:43:45 +0700

    Own sleep

--- internal/parts/gorotunes.go
+++ internal/parts/gorotunes.go
@@ -5,35 +5,13 @@ import (
 	"time"
 )
 
-func pinger(c chan<- string) {
-	for i := 0; ; i++ {
-		c <- "ping"
-	}
-}
-
-func ponger(c chan string) {
-	for {
-		c <- "pong"
-	}
-}
-
-func printer(c <-chan string) {
-	for {
-		msg := <-c
-		fmt.Println(msg)
-		time.Sleep(time.Second)
-	}
-}
-
 func Gorutunes() {
 
-	var c chan string = make(chan string)
-
-	go ponger(c)
-	go pinger(c)
-	go printer(c)
-
-	var input string
-	fmt.Scanln(&input)
+	fmt.Println("tets")
+	Sleep(3)
+	fmt.Println("tets 2")
 }
-	
+
+func Sleep(v int) {
+	<-time.After(time.Second * 2)
+}
