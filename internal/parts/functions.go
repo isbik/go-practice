f6f45d13f1a0492a893cf74cd7425f27707a8fac
Dmitry Medvedew <bikishovd>
2022-02-01 13:53:34 +0700

    Add Functions

--- internal/parts/functions.go
+++ internal/parts/functions.go
@@ -4,21 +4,70 @@ import "fmt"
 
 func Functions() {
 
-	defer func() {
-		str := recover()
-		fmt.Println(str)
+	fmt.Println(half(3))
+	fmt.Println(half(5))
+	fmt.Println(half(6))
 
-		defer first()
-		second()
-	}()
+	fmt.Println("------------------")
 
-	panic("Panic")
+	fmt.Println(max(1, 2, 2, 7, 2, 2))
 
+	fmt.Println("------------------")
+
+	g := makeOddGenerator()
+
+	fmt.Println(g())
+	fmt.Println(g())
+	fmt.Println(g())
+	fmt.Println(g())
+	fmt.Println(g())
+
+	fmt.Println("------------------")
+
+	fmt.Println(fib(4))
+	fmt.Println(fib(9))
+	fmt.Println(fib(2))
+	fmt.Println(fib(1))
 }
 
-func first() {
-	fmt.Println("first")
+func half(num int) (int, bool) {
+
+	return num / 2, num%2 == 0
 }
-func second() {
-	fmt.Println("second")
+
+func max(nums ...int) int {
+	res := nums[0]
+
+	for _, v := range nums {
+
+		if v > res {
+			res = v
+		}
+	}
+
+	return res
+}
+
+func makeOddGenerator() func() int {
+	v := 1
+
+	return func() (ret int) {
+		ret = v
+		v += 2
+		return
+	}
+}
+
+func fib(v int) int {
+
+	if v <= 1 {
+		return 0
+	}
+
+	if v <= 2 {
+		return 1
+	}
+
+	return fib(v-2) + fib(v-1)
+
 }
