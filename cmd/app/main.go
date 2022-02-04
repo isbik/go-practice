36f891923388061a93d74f529fc615c1d2628679
Dmitry Medvedew <bikishovd>
2022-02-04 13:47:24 +0700

    Add vertex

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v *Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func Scale(v *Vertex, f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
 func main() {
+
+	v := Vertex{5, 5}
+
+	Scale(&v, 5)
+
+	fmt.Println(v.Abs())
 
 }
